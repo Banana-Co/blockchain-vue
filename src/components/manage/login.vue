d75d5357f51f5258d3ce3954e044c8e3755cafe7
CastJo <fjchfjch@163.com>
2019-06-14 16:49:05 +0800

    更新

---
+++ src/components/manage/login.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="wrap">
+		<div class="grid-content"><br/><br/><br/><br/><br/><br/><br/></div>
+		<center><el-card class="login-card">
+			<p v-show="showTishi">{{tishi}}</p>
+			<el-input type="text" v-model="loginInfoVo.username" placeholder="请输入用户名"></el-input>
+			<el-input type="password" v-model="loginInfoVo.password" placeholder="请输入密码"></el-input>
+			<br/><el-button type="primary" v-on:click="login">登录</el-button><br/>
+			<div >
+				<span v-on:click="ToRegister">没有账号？马上注册</span><br/>
+			</div>
+		</el-card></center>
+	</div>
+</template>
+
+<script>
+	import {
+		setCookie,
+		getCookie
+	} from '../../assets/js/cookie.js'
+	export default {
+		data() {
+			return {
+				loginInfoVo: {
+					username: '',
+					password: ''
+				},
+				responseResult: [],
+				showTishi: false,
+				tishi: '',
+			}
+		},
+		mounted() {
+			/*页面挂载获取cookie，如果存在username的cookie，则跳转到主页，不需登录*/
+			if (getCookie('username')) {
+				this.$router.push(
+					'/messageFlow'
+				)
+			}
+		},
+		methods: {
+			ToMain() {
+				this.$router.push(
+					'/main'
+				)
+			},
+			ToRegister() {
+				this.$router.push(
+					'/register'
+				)
+			},
+			login() {
+				this.$axios
+					.post('/login', {
+						username: this.loginInfoVo.username,
+						password: this.loginInfoVo.password
+					})
+					.then(successResponse => {
+						this.responseResult = JSON.stringify(successResponse.data)
+						if (successResponse.data.code === 200) {
+							setCookie('username', this.loginInfoVo.username, 1000 * 60)
+								this.$router.push('/messageFlow')
+						} else if (successResponse.data.code === 300) {
+							this.tishi = "该用户不存在"
+							this.showTishi = true
+						} else if (successResponse.data.code === 400) {
+							this.tishi = "密码输入错误"
+							this.showTishi = true
+						}
+					})
+					.catch(failResponse => {})
+
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.wrap {
+		text-align: center;
+	}
+
+	.login-card{
+		width: 320px;
+	}
+
+	.wrap.p {
+		color: red;
+	}
+
+
+	input {
+		display: block;
+		width: 250px;
+		height: 40px;
+		line-height: 40px;
+		margin: 0 auto;
+		margin-bottom: 10px;
+		outline: none;
+		border: 1px solid #888;
+		padding: 10px;
+		box-sizing: border-box;
+		}
+	
+	span {
+		cursor: pointer;
+	}
+
+	span:hover {
+		color: #41b883;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import MyContract from '@/components/MyContract'
+import MessageFlow from '@/components/message/MessageFlow'
+import MessageDetail from '@/components/message/MessageDetail'
+import login from '@/components/manage/login.vue'
+import index from '@/components/home/index.vue'
+import register from '@/components/manage/register.vue'
+import main from '@/components/main/Main.vue'
+import change from '@/components/manage/change.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
     },
     {
-      path: '/myContract',
-      name: 'MyContract',
-      component: MyContract
+      path: '/messageFlow',
+      name: 'MessageFlow',
+      component: MessageFlow
+    },
+    {
+      path: '/messageDetail/:id',
+      name: MessageDetail,
+      component: MessageDetail
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+			{
+			  path: '/register',
+			  name: 'register',
+			  component: register
+			  },
+    {
+      path: '/main',
+      name: 'main',
+      component: main
+    },
+    {
+      path: '/change',
+      name: 'change',
+      component: change
     }
   ]
 })
