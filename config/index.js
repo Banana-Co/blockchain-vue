22ff3755a35a897f8cbdb077b3614f06a7179ba7
CastJo <fjchfjch@163.com>
2019-06-16 00:52:00 +0800

    url

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8060',
+        target: 'http://152.136.103.126:8000',
         changeOrigin: true,
         pathRewrite: {
             '^/api': ''
